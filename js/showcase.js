ba99c64bfa5100f28de3b8c8a58574286ba087aa
RedNinjaX <34778154+RedNinjaX@users.noreply.github.com>
2019-05-15 07:18:33 -0400

    Modified Blog showcase

--- js/showcase.js
+++ js/showcase.js
@@ -1,15 +1,13 @@
 var ShowcaseItem = {
     view: function (vnode) {
         var item = vnode.attrs.item;
+        if (item.poster) item.poster = item.poster.replace('\/s1600\/', '\/w114-h60-c\/');
         var numOfItems = vnode.attrs.numOfItems;
         var index = vnode.attrs.index;
         return m('a', { href: item.url }, [
-            m('div', { class: 'showcase-item animated zoomIn faster', style: convertObjToStyles({ marginBottom: numOfItems > 3 && (index + 1) === numOfItems ? '32px' : undefined, float: numOfItems === 1 ? undefined : index === 0 ? 'left' : 'right', width: numOfItems === 1 || index > 2 ? '100%' : numOfItems === 2 ? '50%' : index === 0 ? '59%' : '40%', height: index === 0 || numOfItems === 2 ? '100%' : '49%', padding: index === 0 ? '0 2px 2px 0' : index === 1 ? '0 0 2px 2px' : index === 2 ? '2px 0 3px 2px' : index === 3 ? '2px 0 0 0' : '4px 0 0 0' }) }, [
-                m('div', { class: 'showcase-content-container', style: convertObjToStyles({ backgroundImage: 'url(' + (item.poster || getPosterWide(item.poster_wide).poster) + ')' }) }, [
-                    m('div', { class: 'showcase-content', style: convertObjToStyles({ height: index === 0 ? '262px' : (index === 1 || index === 3) ? '130px' : '128px' }) }, [
-                        m('span', { class: 'showcase-title' }, item.title)
-                    ])
-                ])
+            m('div', { class: index !== (numOfItems - 1) ? 'bottom-divider' : undefined }, [
+                m('img', { class: 'vertical-center', src: item.poster || getPosterWide(item.poster_wide).poster }),
+                m('span', item.title)
             ])
         ]);
     }
@@ -18,10 +16,11 @@ var ShowcaseItem = {
 var ShowcaseGrid = {
     view: function (vnode) {
         var items = vnode.attrs.items;
-        return m('div', { class: 'showcase-container' },
+        return m('div', { class: 'showcase-container animated zoomIn faster' }, [
+            m('h4', { class: 'poster-header' }, 'From the Blog'),
             items.map(function (item, index) {
                 return m(ShowcaseItem, { key: item.url, item: item, index: index, numOfItems: items.length });
             })
-        );
+        ]);
     }
 };
