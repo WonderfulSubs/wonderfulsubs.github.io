11135184ff87663e9223b9539b27ecc26840e9a9
RedNinjaX <34778154+RedNinjaX@users.noreply.github.com>
2019-06-11 01:00:52 -0400

    Change sidebar entrance animation

--- js/sidebar.js
+++ js/sidebar.js
@@ -14,7 +14,7 @@ function announcementClick() {
 var sidebarStyles = m('style', '.content-wrapper-container.flex{margin:0 auto;flex-direction:column-reverse}@media only screen and (min-width:1000px){.content-wrapper-container.flex{flex-direction:row}}');
 
 var Sidebar = m.fragment({}, [
-    m('div', { class: 'sidebar-container animated zoomIn faster' }, [
+    m('div', { class: 'sidebar-container animated slideInDown' }, [
         m('div', { class: 'sidebar-top-announcement pointer', onclick: announcementClick }),
         // m('div', { id: 'amzn-assoc-ad-29f412c0-c0c8-46e1-9652-1dba8fdf13d5' }),
         // m('script', { async: '', src: '//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=29f412c0-c0c8-46e1-9652-1dba8fdf13d5' }),
