544fd4fcf069aed5985941b17ef633fe07e3b535
RedNinjaX <34778154+RedNinjaX@users.noreply.github.com>
2020-05-31 00:15:10 -0400

    Added footer info to sidebar. Moved inline CSS

--- js/sidebar.js
+++ js/sidebar.js
@@ -14,16 +14,17 @@ function announcementClick() {
 
 var Sidebar = {
     view: function () {
-        var sidebarStyles = m('style', '.content-wrapper-container.flex{margin:0 auto;flex-direction:column-reverse}@media only screen and (min-width:1000px){.content-wrapper-container.flex{flex-direction:row;padding-top:0.3em}}');
+        // var sidebarStyles = m('style', '.content-wrapper-container.flex{margin:0 auto;flex-direction:column-reverse}@media only screen and (min-width:1000px){.content-wrapper-container.flex{flex-direction:row;padding-top:0.3em}}');
         var blogPosts = m(BloggerList, { url: 'https://blog.wonderfulsubs.com/feeds/posts/summary?alt=json&max-results=5' });
 
         return m.fragment({}, [
-            m('div', { class: 'sidebar-container animated slideInDown' }, [
+            m.fragment({}, [
                 llv('div', { class: 'sidebar-top-announcement pointer', onclick: announcementClick }),
                 blogPosts,
+                m('div', { class: 'animated fadeInUp slower' },  '© ' + (new Date()).getFullYear() + ' WonderfulSubs LLC')
                 // llv('iframe', { src: 'https://discordapp.com/widget?id=386361030353354765&theme=light', width: '100%', height: '500', allowtransparency: 'true', frameborder: '0' })
             ]),
-            sidebarStyles
+            // sidebarStyles
         ]);
     }
 };
