605fd605c7ff9df1a70827e1f6fc3e5455e2e73a
RedNinjaX <34778154+RedNinjaX@users.noreply.github.com>
2020-05-31 00:06:24 -0400

    Changed font, colors, added shading

--- js/darkthemecss.js
+++ js/darkthemecss.js
@@ -37,10 +37,7 @@ body{\n\
 .poster-item{\n\
     box-shadow:none\n\
 }\n\
-.post-item{\n\
-    border:1px solid rgba(255,255,255,0.1);\n\
-}\n\
-.post-item,.post-actions>*,.post-show-more-less,.poster-item .poster-title,.showcase-container>div>a>div{\n\
+.post-item,.post-actions>*,.post-show-more-less,.poster-item .poster-title{\n\
     background-color:#424242;\n\
     color:#fff\n\
 }\n\
@@ -53,20 +50,11 @@ body{\n\
 .poster-item .poster-favorite{\n\
     background-color:#151515\n\
 }\n\
-.bottom-bar,.music-player{\n\
-    background-color:#212121\n\
-}\n\
-.bottom-bar-button,.music-player-button{\n\
-    color:#b0b0b0\n\
-}\n\
 .left-divider,.right-divider,.top-divider,.bottom-divider{\n\
     border-color:rgba(189,189,189,0.2)\n\
 }\n\
-.post-actions>*:hover,.bottom-bar-button:hover,.music-player-button:hover{\n\
+.post-actions>*:hover{\n\
     background-color:#484848\n\
-}\n\
-.footer{\n\
-    background-color:#000\n\
 }\n\
 .panel-body{\n\
     background-color:rgba(0,0,0,.75)\n\
@@ -75,23 +63,26 @@ body{\n\
     background-color:#424242\n\
 }\n\
 [type=checkbox]:checked+.checkable:after{\n\
-    color: #ffffff;\n\
+    color: #ffffff\n\
 }\n\
 @media only screen and (max-width:767px){\n\
     .dropbtn.subdrop+div.dropdown-content{\n\
-        border:2px #ffffff solid;\n\
+        border:2px #ffffff solid\n\
     }\n\
     .dropdown-content a:hover{\n\
-        color:#000000;\n\
+        color:#000000\n\
     }\n\
     .dropdown-content,.dropbtn.subdrop:hover+div.dropdown-content,.dropbtn.subdrop+div.dropdown-content:hover,.dropdown-content a,.dropbtn.subdrop,.dropbtn.subdrop+div.dropdown-content a{\n\
         background-color:#000000;\n\
-        color: #ffffff;\n\
-        transition:none;\n\
+        color:#ffffff;\n\
+        transition:none\n\
     }\n\
 }\n\
+.club-content-container{\n\
+    background-color:transparent\n\
+}\n\
 .club-action-buttons>button{\n\
-    border-color:#ffffff;\n\
+    border-color:#ffffff\n\
 }'.replace(/\n^\s*/gm, '');
 
 window.DARK_THEME_STYLES = m('style', DARK_THEME_CSS);
