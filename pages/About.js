d26854cec4d264f79a144671c372177e40e060a9
RedNinjaX <34778154+RedNinjaX@users.noreply.github.com>
2020-08-18 01:01:17 -0400

    Fixed contact and about page layout

--- pages/About.js
+++ pages/About.js
@@ -1,10 +1,15 @@
 var About = {
-    view: function (vnode) {
+    oninit: function () {
+        scrollToTop();
+    },
+    view: function () {
+        setTitle('About', true);
+
         return m.fragment({}, [
             m('div', { class: 'main-container' }, 
                 m('div', { class: 'blog-body' }, [
                     m('h2', { class: 'blog-body-title' }, 'About WonderfulSubs'),
-                    m('div', { class: 'blog-body-content bottom-divider' }, 'Abc'),
+                    m('div', { class: 'blog-body-content bottom-divider' }, ''),
                 ])
             ),
             m('style', '@media only screen and (max-width:998px){.content-wrapper-container.flex{overflow:hidden}}')
